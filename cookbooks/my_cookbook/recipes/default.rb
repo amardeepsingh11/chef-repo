391364d7a9bbcdab0853bec004e6aab9f04f1053
Amardeep Singh <amar.sahdev@gmail.com>
2017-11-19 21:12:08 +1100

    Updated few files

---
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook:: my_cookbook
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+file "/tmp/local_mode.txt" do
+
+content "created by chef client local mode"
+
+end
